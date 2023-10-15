455cd46b1335ab8ffc00ffd4053fff368faa804c
M Nindra Zaka <mnindrazaka@gmail.com>
2023-10-15 19:51:10 +0700

    feat(vanilla-spa): implement component

--- vanilla-spa/index.js
+++ vanilla-spa/index.js
@@ -1,56 +1,75 @@
-const root = document.getElementById("root");
+function Navbar() {
+  const linkHome = document.createElement("a");
+  linkHome.href = "#home";
+  linkHome.textContent = "Home";
+  linkHome.onclick = function (event) {
+    event.preventDefault();
+    window.history.pushState(null, "", event.target.href);
+    render();
+  };
 
-const title = document.createElement("h1");
-title.textContent = "Home";
+  const linkAbout = document.createElement("a");
+  linkAbout.href = "#about";
+  linkAbout.textContent = "About";
+  linkAbout.onclick = function (event) {
+    event.preventDefault();
+    window.history.pushState(null, "", event.target.href);
+    render();
+  };
 
-const linkHome = document.createElement("a");
-linkHome.href = "#home";
-linkHome.textContent = "Home";
-
-const searchInput = document.createElement("input");
-searchInput.placeholder = "input your product name";
-
-linkHome.onclick = function (event) {
-  root.innerHTML = "";
-
-  event.preventDefault();
-
-  root.appendChild(linkHome);
-  root.appendChild(linkAbout);
-  root.appendChild(title);
-  title.textContent = "Home";
-
-  root.appendChild(searchInput);
-
-  window.history.pushState(null, "", event.target.href);
-};
-
-const linkAbout = document.createElement("a");
-linkAbout.href = "#about";
-linkAbout.textContent = "About";
-
-linkAbout.onclick = function (event) {
-  root.innerHTML = "";
-
-  event.preventDefault();
-
-  root.appendChild(linkHome);
-  root.appendChild(linkAbout);
-  root.appendChild(title);
-  title.textContent = "About";
-  window.history.pushState(null, "", event.target.href);
-};
-
-root.appendChild(linkHome);
-root.appendChild(linkAbout);
-root.appendChild(title);
-
-if (window.location.hash === "#home") {
-  title.textContent = "Home";
-  root.appendChild(searchInput);
-} else if (window.location.hash === "#about") {
-  title.textContent = "About";
-} else {
-  title.textContent = "Home";
-  root.appendChild(searchInput);
+  const container = document.createElement("div");
+  container.appendChild(linkHome);
+  container.appendChild(linkAbout);
+  return container;
 }
+
+function HomePage() {
+  const navbar = Navbar();
+
+  const title = document.createElement("h1");
+  title.textContent = "Home";
+
+  const searchInput = document.createElement("input");
+  searchInput.placeholder = "input your product name";
+
+  const container = document.createElement("div");
+  container.appendChild(navbar);
+  container.appendChild(title);
+  container.appendChild(searchInput);
+
+  return container;
+}
+
+function AboutPage() {
+  const navbar = Navbar();
+
+  const title = document.createElement("h1");
+  title.textContent = "About";
+
+  const container = document.createElement("div");
+  container.appendChild(navbar);
+  container.appendChild(title);
+
+  return container;
+}
+
+function App() {
+  const homePage = HomePage();
+  const aboutPage = AboutPage();
+
+  if (window.location.hash === "#home") {
+    return homePage;
+  } else if (window.location.hash === "#about") {
+    return aboutPage;
+  } else {
+    return homePage;
+  }
+}
+
+function render() {
+  const root = document.getElementById("root");
+  root.innerHTML = "";
+  root.appendChild(App());
+}
+
+render();
