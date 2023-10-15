2525b9fe4cbbcd48a5718714bf21a9d8d72c2629
M Nindra Zaka <mnindrazaka@gmail.com>
2023-10-15 17:14:05 +0700

    feat(vanilla-spa): implement client side rendering

--- vanilla-spa/index.js
+++ vanilla-spa/index.js
@@ -1,23 +1,56 @@
-const linkHome = document.getElementById("link-home");
-const linkAbout = document.getElementById("link-about");
-const title = document.getElementById("title");
+const root = document.getElementById("root");
 
-linkAbout.onclick = function (event) {
-  event.preventDefault();
-  title.textContent = "About Page";
-  window.history.pushState(null, "", event.target.href);
-};
+const title = document.createElement("h1");
+title.textContent = "Home";
+
+const linkHome = document.createElement("a");
+linkHome.href = "#home";
+linkHome.textContent = "Home";
+
+const searchInput = document.createElement("input");
+searchInput.placeholder = "input your product name";
 
 linkHome.onclick = function (event) {
+  root.innerHTML = "";
+
   event.preventDefault();
-  title.textContent = "Home Page";
+
+  root.appendChild(linkHome);
+  root.appendChild(linkAbout);
+  root.appendChild(title);
+  title.textContent = "Home";
+
+  root.appendChild(searchInput);
+
   window.history.pushState(null, "", event.target.href);
 };
 
+const linkAbout = document.createElement("a");
+linkAbout.href = "#about";
+linkAbout.textContent = "About";
+
+linkAbout.onclick = function (event) {
+  root.innerHTML = "";
+
+  event.preventDefault();
+
+  root.appendChild(linkHome);
+  root.appendChild(linkAbout);
+  root.appendChild(title);
+  title.textContent = "About";
+  window.history.pushState(null, "", event.target.href);
+};
+
+root.appendChild(linkHome);
+root.appendChild(linkAbout);
+root.appendChild(title);
+
 if (window.location.hash === "#home") {
-  title.textContent = "Home Page";
+  title.textContent = "Home";
+  root.appendChild(searchInput);
 } else if (window.location.hash === "#about") {
-  title.textContent = "About Page";
+  title.textContent = "About";
 } else {
-  title.textContent = "Home Page ku";
+  title.textContent = "Home";
+  root.appendChild(searchInput);
 }
